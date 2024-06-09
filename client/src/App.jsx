6f63e68aa68cc1f67ac04cd809934020ddfddf6c
animesh-chakrabarty <animesh_chakrabarty@outlook.com>
2024-06-09 15:05:27 +0530

    initial setup - 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import { Button } from "./components/ui/button";
+import { Routes, Route } from "react-router-dom";
+// importing components
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div>
-      <Button variant="destructive"> Click me </Button>
-    </div>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard/:id" element={<Dashboard />} />
+      </Routes>
+    </main>
   );
 }
 
