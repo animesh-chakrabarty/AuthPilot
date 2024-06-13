33bbece66f6f9229d7eb4a3e4e1ee010ef63d2ae
animesh-chakrabarty <animesh_chakrabarty@outlook.com>
2024-06-13 17:29:50 +0530

    user & transaction schema added

---
+++ server/src/models/transaction.models.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: [String],
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("transaction", transactionSchema);
